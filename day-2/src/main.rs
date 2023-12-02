6e00e5a90aa1df143873f1d65d608481c4dc2618
Vladimir Yakovlev <nagos@inbox.ru>
2023-12-03 00:11:35 +0300

    Refactoring

--- day-2/src/main.rs
+++ day-2/src/main.rs
@@ -3,6 +3,11 @@ use std::fs;
 #[derive(Debug)]
 struct Game {
     id: u32,
+    sets: Vec<GameSet>,
+}
+
+#[derive(Debug, Default)]
+struct GameSet {
     red: u32,
     green: u32,
     blue: u32,
@@ -12,78 +17,82 @@ impl Game {
     fn parse(s: &str) -> Self {
         let (game, game_data) = s.split_once(": ").unwrap();
         let id = game.strip_prefix("Game ").unwrap().parse::<u32>().unwrap();
-        let sets = game_data.split("; ").map(GameSet::parse);
-        let mut red = 0;
-        let mut green = 0;
-        let mut blue = 0;
-        for set in sets {
-            red = red.max(set.red);
-            green = green.max(set.green);
-            blue = blue.max(set.blue);
-        }
-        Game {
-            id,
-            red,
-            green,
-            blue,
-        }
+        let sets = game_data
+            .split("; ")
+            .map(GameSet::parse)
+            .collect::<Vec<GameSet>>();
+        Game { id, sets }
     }
 
-    fn check(&self, red: u32, green: u32, blue: u32) -> bool {
-        self.red <= red && self.green <= green && self.blue <= blue
+    fn max_set(&self) -> GameSet {
+        let mut ret = GameSet::default();
+        for set in &self.sets {
+            ret.red = ret.red.max(set.red);
+            ret.green = ret.green.max(set.green);
+            ret.blue = ret.blue.max(set.blue);
+        }
+
+        ret
+    }
+
+    fn check(&self, set_limit: &GameSet) -> bool {
+        let max_set = self.max_set();
+        max_set.red <= set_limit.red
+            && max_set.green <= set_limit.green
+            && max_set.blue <= set_limit.blue
     }
 
     fn power(&self) -> u32 {
-        self.red * self.green * self.blue
+        let max_set = self.max_set();
+        max_set.red * max_set.green * max_set.blue
     }
-}
-
-#[derive(Debug)]
-struct GameSet {
-    red: u32,
-    green: u32,
-    blue: u32,
 }
 
 impl GameSet {
     fn parse(s: &str) -> Self {
-        let mut red = 0;
-        let mut green = 0;
-        let mut blue = 0;
+        let mut ret = GameSet::default();
 
         let cubes = s.split(", ");
         for cube in cubes {
             let set = cube.split(' ').collect::<Vec<&str>>();
             let count = set[0].parse::<u32>().unwrap();
             let color = set[1];
-            match color {
-                "red" => red += count,
-                "green" => green += count,
-                "blue" => blue += count,
+            let c = match color {
+                "red" => &mut ret.red,
+                "green" => &mut ret.green,
+                "blue" => &mut ret.blue,
                 &_ => unreachable!(),
-            }
+            };
+            *c += count;
         }
-        GameSet { red, green, blue }
+        ret
     }
 }
 
 fn main() {
     let data = fs::read_to_string("data/day-2-input.txt").unwrap();
 
-    let answer_one = proc(&data, 12, 13, 14);
+    let answer_one = proc(
+        &data,
+        &GameSet {
+            red: 12,
+            green: 13,
+            blue: 14,
+        },
+    );
     println!("Day 2 part one: {answer_one}");
 
     let answer_two = proc_2(&data);
     println!("Day 2 part two: {answer_two}");
 }
 
-fn proc(data: &str, red: u32, green: u32, blue: u32) -> u32 {
-    data.lines().map(|l| proc_line(l, red, green, blue)).sum()
+fn proc(data: &str, set_limit: &GameSet) -> u32 {
+    data.lines().map(|l| proc_line(l, set_limit)).sum()
 }
 
-fn proc_line(line: &str, red: u32, green: u32, blue: u32) -> u32 {
+fn proc_line(line: &str, set_limit: &GameSet) -> u32 {
     let game = Game::parse(line);
-    if game.check(red, green, blue) {
+    if game.check(set_limit) {
         game.id
     } else {
         0
@@ -103,17 +112,29 @@ mod tests {
         let data = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green";
         let game = Game::parse(data);
         assert_eq!(game.id, 1);
-        assert_eq!(game.red, 4);
-        assert_eq!(game.green, 2);
-        assert_eq!(game.blue, 6);
-        assert!(game.check(4, 2, 6));
+        let max_set = game.max_set();
+        assert_eq!(max_set.red, 4);
+        assert_eq!(max_set.green, 2);
+        assert_eq!(max_set.blue, 6);
+        assert!(game.check(&GameSet {
+            red: 4,
+            green: 2,
+            blue: 6
+        }));
         assert_eq!(game.power(), 48);
     }
 
     #[test]
     fn test_file() {
         let data = fs::read_to_string("data/day-2-test.txt").unwrap();
-        let res = proc(&data, 12, 13, 14);
+        let res = proc(
+            &data,
+            &GameSet {
+                red: 12,
+                green: 13,
+                blue: 14,
+            },
+        );
         assert_eq!(res, 8);
 
         let res_2 = proc_2(&data);
