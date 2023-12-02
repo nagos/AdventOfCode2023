610cf9bf08b4da98975b7d9b06844a641870206c
Vladimir Yakovlev <nagos@inbox.ru>
2023-12-02 14:21:19 +0300

    Day 1 part two

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -1,34 +1,50 @@
-use std::fs;
+use std::{fs, vec};
+
+const DIGITS: &[&str] = &[
+    "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+];
 
 fn main() {
     let data = fs::read_to_string("data/day-1-input.txt").unwrap();
-    let answer = proc(&data);
-    println!("Day 1 aswer: {answer}")
+    let answer_1 = proc(&data, false);
+    println!("Day 1 part 1: {answer_1}");
+
+    let answer_2 = proc(&data, true);
+    println!("Day 1 part 2: {answer_2}");
 }
 
-fn proc_line(line: &str) -> u32 {
+fn check_digit(line: &str) -> Option<u32> {
+    line.chars().next().unwrap().to_digit(10)
+}
+
+fn check_letters(line: &str) -> Option<u32> {
+    for (i, v) in DIGITS.iter().enumerate() {
+        if line.starts_with(v) {
+            return Some((i + 1) as u32);
+        }
+    }
+    None
+}
+
+fn proc_line(line: &str, words: bool) -> u32 {
+    let mut tmp = line;
     let mut store = vec![];
-    for i in line.chars() {
-        if let Some(d) = i.to_digit(10) {
+    while !tmp.is_empty() {
+        if let Some(d) = check_digit(tmp) {
             store.push(d);
+        } else if words {
+            if let Some(d) = check_letters(tmp) {
+                store.push(d);
+            }
         }
+
+        tmp = &tmp[1..];
     }
-
-    let ret = store.first().unwrap() * 10 + store.last().unwrap();
-
-    ret
+    store.first().unwrap() * 10 + store.last().unwrap()
 }
 
-fn proc(data: &str) -> u32 {
-    let mut ret = 0;
-    for l in data.lines() {
-        if !l.is_empty() {
-            let d = proc_line(l);
-            ret += d;
-        }
-    }
-
-    ret
+fn proc(data: &str, letters: bool) -> u32 {
+    data.lines().map(|l| proc_line(l, letters)).sum()
 }
 
 #[cfg(test)]
@@ -37,14 +53,27 @@ mod tests {
 
     #[test]
     fn line_test() {
-        assert_eq!(proc_line("1abc2"), 12);
-        assert_eq!(proc_line("pqr3stu8vwx"), 38);
-        assert_eq!(proc_line("a1b2c3d4e5f"), 15);
-        assert_eq!(proc_line("treb7uchet"), 77);
+        assert_eq!(proc_line("1abc2", false), 12);
+        assert_eq!(proc_line("pqr3stu8vwx", false), 38);
+        assert_eq!(proc_line("a1b2c3d4e5f", false), 15);
+        assert_eq!(proc_line("treb7uchet", false), 77);
     }
+
     #[test]
     fn test_file() {
         let data = fs::read_to_string("data/day-1-test.txt").unwrap();
-        assert_eq!(proc(&data), 142);
+        assert_eq!(proc(&data, false), 142);
+    }
+
+    #[test]
+    fn test_word_digits() {
+        assert_eq!(proc_line("two1nine", true), 29);
+        assert_eq!(proc_line("eightwothree", true), 83);
+        assert_eq!(proc_line("abcone2threexyz", true), 13);
+        assert_eq!(proc_line("xtwone3four", true), 24);
+        assert_eq!(proc_line("4nineeightseven2", true), 42);
+        assert_eq!(proc_line("zoneight234", true), 14);
+        assert_eq!(proc_line("7pqrstsixteen", true), 76);
+        assert_eq!(proc_line("twoneight", true), 28);
     }
 }
