21583339935a31c119168a8dbda7ba42d5c59f82
Vladimir Yakovlev <nagos@inbox.ru>
2023-12-04 18:27:40 +0300

    Dedicated parser for padded int

--- day-4/src/main.rs
+++ day-4/src/main.rs
@@ -1,11 +1,11 @@
 use nom::bytes::complete::tag;
-use nom::character::complete::{alpha1, digit1, space1};
+use nom::character::complete::{digit1, space0, space1};
 use nom::combinator::map_res;
 use nom::multi::separated_list1;
 use nom::IResult;
 use std::collections::HashSet;
+use std::fs;
 use std::iter::FromIterator;
-use std::{fs, vec};
 
 fn main() {
     let data = fs::read_to_string("data/input.txt").unwrap();
@@ -13,18 +13,18 @@ fn main() {
     println!("Day 4 tart one: {total}");
 }
 
+fn digit1_padded(input: &str) -> IResult<&str, u32> {
+    let (input, _) = space0(input)?;
+    map_res(digit1, |s: &str| s.parse::<u32>())(input)
+}
+
 fn parse_line(input: &str) -> IResult<&str, (u32, Vec<u32>, Vec<u32>)> {
-    let (input, _) = tag("Card")(input)?;
-    let (input, _) = space1(input)?;
-    let (input, card_id) = map_res(digit1, |s| u32::from_str_radix(s, 10))(input)?;
-    let (input, _) = tag(":")(input)?;
-    let (input, _) = space1(input)?;
-    let (input, numbers_winning) =
-        separated_list1(space1, map_res(digit1, |s: &str| s.parse()))(input)?;
-    let (input, _) = tag(" |")(input)?;
-    let (input, _) = space1(input)?;
-    let (input, numbers_have) =
-        separated_list1(space1, map_res(digit1, |s: &str| s.parse()))(input)?;
+    let (input, _) = tag("Card ")(input)?;
+    let (input, card_id) = digit1_padded(input)?;
+    let (input, _) = tag(": ")(input)?;
+    let (input, numbers_winning) = separated_list1(space1, digit1_padded)(input)?;
+    let (input, _) = tag(" | ")(input)?;
+    let (input, numbers_have) = separated_list1(space1, digit1_padded)(input)?;
 
     Ok((input, (card_id, numbers_winning, numbers_have)))
 }
@@ -35,18 +35,16 @@ fn calc_points(numbers_have: Vec<u32>, numbers_winning: Vec<u32>) -> u32 {
     let intersect = have.intersection(&winning);
     let count = intersect.count();
 
-    let score = match count as u32 {
+    match count as u32 {
         0 => 0,
         x => 2_u32.pow(x - 1),
-    };
-
-    score
+    }
 }
 
 fn proc(data: &str) -> u32 {
     let mut ret = 0;
     for input in data.lines() {
-        let (_, (card_id, numbers_winning, numbers_have)) = parse_line(input).unwrap();
+        let (_, (_card_id, numbers_winning, numbers_have)) = parse_line(input).unwrap();
         let score = calc_points(numbers_have, numbers_winning);
         ret += score;
     }
@@ -85,5 +83,13 @@ mod tests {
         let data = fs::read_to_string("data/test.txt").unwrap();
         let total = proc(&data);
         assert_eq!(total, 13);
+    }
+
+    #[test]
+    fn test_parser_padded() {
+        let data = " 123";
+        let (input, value) = digit1_padded(data).unwrap();
+        assert!(input.is_empty());
+        assert_eq!(value, 123);
     }
 }
