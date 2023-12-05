398312b694a8ad507188ec845f700f71e04062b1
Vladimir Yakovlev <nagos@inbox.ru>
2023-12-05 23:04:43 +0300

    Refactored nom parser

--- day-5/src/main.rs
+++ day-5/src/main.rs
@@ -4,8 +4,7 @@ use nom::{
     bytes::complete::tag,
     character::complete::{digit1, newline, not_line_ending, space1},
     combinator::map_res,
-    multi::{count, separated_list1},
-    sequence::{pair, terminated},
+    multi::{separated_list1, many0, many1},
     IResult,
 };
 
@@ -29,33 +28,30 @@ fn digit1_u32(input: &str) -> IResult<&str, u32> {
 fn seeds_parser(input: &str) -> IResult<&str, Vec<u32>> {
     let (input, _) = tag("seeds: ")(input)?;
     let (input, seeds) = separated_list1(space1, digit1_u32)(input)?;
-    let (input, _) = count(newline, 2)(input)?;
+    let (input, _) = many1(newline)(input)?;
 
     Ok((input, seeds))
 }
 
 fn map_item_parser(input: &str) -> IResult<&str, TableItem> {
     let (input, data) = separated_list1(space1, digit1_u32)(input)?;
+    let (input, _) = newline(input)?;
 
     Ok((input, (data[0], data[1], data[2])))
 }
 
 fn block_parser(input: &str) -> IResult<&str, MapBlock> {
-    let (input, _) = terminated(not_line_ending, newline)(input)?;
-    let (input, data) = separated_list1(newline, map_item_parser)(input)?;
+    let (input, _) = not_line_ending(input)?;
+    let (input, _) = newline(input)?;
+    let (input, data) = many1(map_item_parser)(input)?;
+    let (input, _) = many0(newline)(input)?;
 
-    Ok((input, data))
-}
-
-fn block_list_parser(input: &str) -> IResult<&str, Vec<MapBlock>> {
-    let (input, data) = separated_list1(pair(newline, newline), block_parser)(input)?;
     Ok((input, data))
 }
 
 fn map_parser(input: &str) -> IResult<&str, (Vec<u32>, Vec<MapBlock>)> {
     let (input, seeds) = seeds_parser(input)?;
-    let (input, blocks) = block_list_parser(input)?;
-    let (input, _) = newline(input)?;
+    let (input, blocks) = many1(block_parser)(input)?;
 
     Ok((input, (seeds, blocks)))
 }
@@ -168,17 +164,24 @@ mod tests {
 
     #[test]
     fn test_block_parser() {
-        let data = "seed-to-soil map:\n50 98 2\n52 50 48";
+        let data = "seed-to-soil map:\n50 98 2\n52 50 48\n\n";
         let (input, _) = block_parser(data).unwrap();
         assert!(input.is_empty());
     }
 
     #[test]
     fn test_map_item_parser() {
-        let data = "49 53 8";
+        let data = "49 53 8\n";
         let (input, values) = map_item_parser(data).unwrap();
         assert_eq!(values, (49, 53, 8));
         assert!(input.is_empty());
+    }
+
+    #[test]
+    fn test_map_parser() {
+        let data = fs::read_to_string("data/test.txt").unwrap();
+        let (intput,_) = map_parser(&data).unwrap();
+        assert!(intput.is_empty());
     }
 
     #[test]
