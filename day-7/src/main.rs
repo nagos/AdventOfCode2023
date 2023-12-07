60ce4898a0c4ef361e59594b502a904ac6f5df5f
Vladimir Yakovlev <nagos@inbox.ru>
2023-12-07 18:22:32 +0300

    Day 7 part two

--- day-7/src/main.rs
+++ day-7/src/main.rs
@@ -10,9 +10,20 @@ use std::fs;
 
 fn main() {
     let data = fs::read_to_string("data/input.txt").unwrap();
-    let part_one = proc1(&data);
+    let part_one = proc(&data, false);
     println!("Day 7 part one: {part_one}");
+
+    let part_two = proc(&data, true);
+    println!("Day 7 part two: {part_two}");
 }
+
+const CARDS: &[&str] = &[
+    "A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2",
+];
+
+const CARDS2: &[&str] = &[
+    "A", "K", "Q", "T", "9", "8", "7", "6", "5", "4", "3", "2", "J",
+];
 
 fn digit1_u32(input: &str) -> IResult<&str, u32> {
     map_res(digit1, |s: &str| s.parse::<u32>())(input)
@@ -30,25 +41,33 @@ fn parse(input: &str) -> IResult<&str, Vec<(Vec<&str>, u32)>> {
     many1(parse_line)(input)
 }
 
-const CARDS: &[&str] = &[
-    "A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2",
-];
-
-fn card_to_int(card: &str) -> u32 {
-    CARDS.iter().position(|&x| x == card).unwrap() as u32
+fn card_to_int(card: &str, use_jockers: bool) -> u32 {
+    let table = if !use_jockers { CARDS } else { CARDS2 };
+    table.iter().position(|&x| x == card).unwrap() as u32
 }
 
-fn process_hand(hand: (Vec<&str>, u32)) -> (Vec<u32>, u32) {
-    let cards = hand.0.into_iter().map(card_to_int).collect::<Vec<u32>>();
-    (cards, hand.1)
+fn process_hand(cards: Vec<&str>, use_jockers: bool) -> Vec<u32> {
+    cards
+        .into_iter()
+        .map(|x| card_to_int(x, use_jockers))
+        .collect()
 }
 
-fn get_hand_score(hand: Vec<u32>) -> Vec<u32> {
+fn is_jocker(card: u32) -> bool {
+    card == CARDS2.len() as u32 - 1
+}
+
+fn get_hand_score(hand: Vec<u32>, use_jockers: bool) -> Vec<u32> {
     let mut card_types = vec![0; CARDS.len()];
     let mut ret = hand.clone();
 
+    let mut jokers = 0;
     for c in hand {
-        card_types[c as usize] += 1;
+        if use_jockers && is_jocker(c) {
+            jokers += 1;
+        } else {
+            card_types[c as usize] += 1;
+        }
     }
 
     // 5 4 3 2
@@ -66,7 +85,7 @@ fn get_hand_score(hand: Vec<u32>) -> Vec<u32> {
         }
     }
 
-    let score = match doubles {
+    let mut score = match doubles {
         (1, _, _, _) => 0, // Five of a kind
         (_, 1, _, _) => 1, // Four of a kind
         (_, _, 1, 1) => 2, // Full house
@@ -75,31 +94,48 @@ fn get_hand_score(hand: Vec<u32>) -> Vec<u32> {
         (_, _, _, 1) => 5, // One pair
         _ => 6,            // High card
     };
+
+    score = match (jokers, score) {
+        (0, _) => score,
+        (1, 6) => 5,
+        (1, 5) => 3,
+        (1, 4) => 2,
+        (1, 3) => 1,
+        (1, 1) => 0,
+        (2, 6) => 3,
+        (2, 5) => 1,
+        (2, 3) => 0,
+        (3, 6) => 1,
+        (3, 5) => 0,
+        (4, 6) => 0,
+        (5, _) => 0,
+        _ => unreachable!(),
+    };
+
     ret.insert(0, score);
 
     ret
 }
 
-fn proc1(data: &str) -> u32 {
+fn proc(data: &str, use_jockers: bool) -> u32 {
     let (_, data) = parse(data).unwrap();
 
-    let mut bids = vec![];
+    let mut hands = vec![];
 
     for (cards, bid) in data {
-        let hand = process_hand((cards, bid));
-        let hand_score = get_hand_score(hand.0);
-        bids.push((hand_score, bid));
+        let hand = process_hand(cards, use_jockers);
+        let hand_score = get_hand_score(hand, use_jockers);
+        hands.push((hand_score, bid));
     }
 
-    bids.sort();
-    bids.reverse();
+    hands.sort();
 
-    let mut ret = 0;
-    for (i, b) in bids.iter().enumerate() {
-        ret += b.1 * (i as u32 + 1);
-    }
-
-    ret
+    hands
+        .iter()
+        .rev()
+        .enumerate()
+        .map(|(i, b)| b.1 * (i as u32 + 1))
+        .sum()
 }
 
 #[cfg(test)]
@@ -115,9 +151,20 @@ mod test {
         assert!(input.is_empty());
         assert_eq!(bid, 765);
 
-        let hand = process_hand((cards, bid));
-        let hand_score = get_hand_score(hand.0);
+        let hand = process_hand(cards, false);
+        let hand_score = get_hand_score(hand, false);
         assert_eq!(hand_score, vec![5, 11, 12, 4, 11, 1]);
+    }
+
+    #[test]
+    fn test_parse2() {
+        let data = "T55J5 684\n";
+
+        let (_, (cards, _bid)) = parse_line(data).unwrap();
+
+        let hand = process_hand(cards, true);
+        let hand_score = get_hand_score(hand, true);
+        assert_eq!(hand_score, vec![1, 3, 8, 8, 12, 8]);
     }
 
     #[test]
@@ -131,7 +178,14 @@ mod test {
     #[test]
     fn test_proc1() {
         let data = fs::read_to_string("data/test.txt").unwrap();
-        let res = proc1(&data);
+        let res = proc(&data, false);
         assert_eq!(res, 6440);
+    }
+
+    #[test]
+    fn test_proc2() {
+        let data = fs::read_to_string("data/test.txt").unwrap();
+        let res = proc(&data, true);
+        assert_eq!(res, 5905);
     }
 }
