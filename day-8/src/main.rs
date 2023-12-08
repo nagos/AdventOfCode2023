00703fdf89bd5d462425cd547d68bc7c6fa2ace9
Vladimir Yakovlev <nagos@inbox.ru>
2023-12-08 20:29:25 +0300

    Part two, brute force method. Does not work.

--- day-8/src/main.rs
+++ day-8/src/main.rs
@@ -1,6 +1,6 @@
 use nom::{
     bytes::complete::tag,
-    character::complete::{alpha1, newline, one_of},
+    character::complete::{alphanumeric1, newline, one_of},
     multi::many1,
     sequence::terminated,
     IResult,
@@ -11,6 +11,9 @@ fn main() {
     let data = fs::read_to_string("data/input.txt").unwrap();
     let part_one = proc_1(&data);
     println!("Day 8 part one: {part_one}");
+
+    let part_two = proc_2(&data);
+    println!("Day 8 part two: {part_two}");
 }
 
 type MapNode<'a> = (&'a str, &'a str, &'a str);
@@ -23,11 +26,11 @@ fn parse_direction(input: &str) -> IResult<&str, Vec<char>> {
 }
 
 fn parse_node(input: &str) -> IResult<&str, MapNode> {
-    let (input, node) = alpha1(input)?;
+    let (input, node) = alphanumeric1(input)?;
     let (input, _) = tag(" = (")(input)?;
-    let (input, edge_l) = alpha1(input)?;
+    let (input, edge_l) = alphanumeric1(input)?;
     let (input, _) = tag(", ")(input)?;
-    let (input, edge_r) = alpha1(input)?;
+    let (input, edge_r) = alphanumeric1(input)?;
     let (input, _) = tag(")\n")(input)?;
 
     Ok((input, (node, edge_l, edge_r)))
@@ -40,26 +43,24 @@ fn parse(input: &str) -> IResult<&str, (Vec<char>, Vec<MapNode>)> {
     Ok((input, (directions, nodes)))
 }
 
-fn calc(directions: Vec<char>, map_data: Vec<MapNode>) -> u32 {
+fn build_map(map_data: Vec<MapNode>) -> HashMap<&str, (&str, &str)> {
     let mut map = HashMap::new();
 
     for m in map_data {
         map.insert(m.0, (m.1, m.2));
     }
 
+    map
+}
+
+fn calc_1(directions: Vec<char>, map: HashMap<&str, (&str, &str)>) -> u32 {
     let mut curren_key = "AAA";
-    let mut current_node = map.get(curren_key).unwrap();
 
     for i in 0.. {
         let idx = i % directions.len();
         let lr = directions[idx];
 
-        if lr == 'L' {
-            curren_key = current_node.0;
-        } else {
-            curren_key = current_node.1;
-        }
-        current_node = map.get(curren_key).unwrap();
+        curren_key = node_traverse(lr, curren_key, &map);
 
         if curren_key == "ZZZ" {
             return i as u32 + 1;
@@ -70,7 +71,59 @@ fn calc(directions: Vec<char>, map_data: Vec<MapNode>) -> u32 {
 
 fn proc_1(data: &str) -> u32 {
     let (_, (directions, nodes)) = parse(data).unwrap();
-    calc(directions, nodes)
+    let map = build_map(nodes);
+    calc_1(directions, map)
+}
+
+fn find_starting_nodes<'a>(map: &HashMap<&'a str, (&'a str, &'a str)>) -> Vec<&'a str> {
+    let mut nodes = vec![];
+    for n in map.keys() {
+        if n.ends_with('A') {
+            nodes.push(*n);
+        }
+    }
+
+    nodes
+}
+
+fn node_traverse<'a>(
+    lr: char,
+    node: &'a str,
+    map: &HashMap<&'a str, (&'a str, &'a str)>,
+) -> &'a str {
+    let current_node = map.get(node).unwrap();
+
+    if lr == 'L' {
+        current_node.0
+    } else {
+        current_node.1
+    }
+}
+
+fn calc_2(directions: Vec<char>, map: HashMap<&str, (&str, &str)>) -> u32 {
+    let mut path_nodes = find_starting_nodes(&map);
+
+    for i in 0.. {
+        let idx = i % directions.len();
+        let lr = directions[idx];
+
+        path_nodes = path_nodes
+            .iter()
+            .map(|node| node_traverse(lr, node, &map))
+            .collect();
+
+        let end = path_nodes.iter().map(|n| n.ends_with('Z')).all(|end| end);
+        if end {
+            return i as u32 + 1;
+        }
+    }
+    unreachable!()
+}
+
+fn proc_2(data: &str) -> u32 {
+    let (_, (directions, nodes)) = parse(data).unwrap();
+    let map = build_map(nodes);
+    calc_2(directions, map)
 }
 
 #[cfg(test)]
@@ -83,7 +136,8 @@ mod test {
         let (input, (directions, nodes)) = parse(&data).unwrap();
         assert!(input.is_empty());
 
-        let res = calc(directions, nodes);
+        let map = build_map(nodes);
+        let res = calc_1(directions, map);
         dbg!(res);
     }
 
@@ -113,6 +167,13 @@ mod test {
 
         let data_2 = fs::read_to_string("data/test_2.txt").unwrap();
         let res = proc_1(&data_2);
+        assert_eq!(res, 6);
+    }
+
+    #[test]
+    fn test_calc_2() {
+        let data = fs::read_to_string("data/test_3.txt").unwrap();
+        let res = proc_2(&data);
         assert_eq!(res, 6);
     }
 }
