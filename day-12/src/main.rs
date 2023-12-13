deb6262f86fb7398e3513d54a4dee7bfc71f2c65
Vladimir Yakovlev <nagos@inbox.ru>
2023-12-13 12:58:06 +0300

    Optimized recursion with no allocations

--- day-12/src/main.rs
+++ day-12/src/main.rs
@@ -5,8 +5,8 @@ use nom::{
     IResult,
 };
 
-use std::{fs, vec};
 use std::time::Instant;
+use std::{fs, vec};
 
 type LineData = (Vec<char>, Vec<u32>);
 
@@ -39,6 +39,7 @@ fn parse(input: &str) -> IResult<&str, Vec<LineData>> {
     many1(parse_line)(input)
 }
 
+#[allow(dead_code)]
 fn calc_list(data: &Vec<char>) -> Vec<u32> {
     let mut tmp = 0;
     let mut res = vec![];
@@ -58,33 +59,59 @@ fn calc_list(data: &Vec<char>) -> Vec<u32> {
     res
 }
 
-fn solve(data: Vec<char>, unknown_list: Vec<u32>, list: &Vec<u32>) -> u32 {
+fn verify_list(data: &Vec<char>, list: &Vec<u32>) -> bool {
+    let mut idx = 0;
+    let mut tmp = 0;
+
+    for c in data {
+        if *c == '#' {
+            tmp += 1;
+        } else if tmp != 0 {
+            if idx >= list.len() || tmp != list[idx] {
+                return false;
+            }
+            tmp = 0;
+            idx += 1;
+        }
+    }
+
+    if tmp != 0 {
+        if idx >= list.len() || tmp != list[idx] {
+            return false;
+        }
+        idx += 1;
+    }
+
+    if idx != list.len() {
+        return false;
+    }
+
+    true
+}
+
+fn solve(data: &mut Vec<char>, unknown_list: &Vec<usize>, idx: usize, list: &Vec<u32>) -> u32 {
     let mut ret = 0;
 
-    if !unknown_list.is_empty() {
-        let mut new_data = data.clone();
-        let mut new_unknown_list = unknown_list.clone();
-        let idx = new_unknown_list.pop().unwrap();
-        new_data[idx as usize] = '#';
-        ret += solve(new_data.clone(), new_unknown_list.clone(), list);
-        new_data[idx as usize] = '.';
-        ret += solve(new_data, new_unknown_list, list);
-    } else {
-        let res_list = calc_list(&data);
-        if res_list == *list {
-            ret = 1;
-        }
+    if idx < unknown_list.len() {
+        let char_idx = unknown_list[idx];
+        data[char_idx] = '#';
+        ret += solve(data, unknown_list, idx + 1, list);
+        data[char_idx] = '.';
+        ret += solve(data, unknown_list, idx + 1, list);
+        data[char_idx] = '?';
+    } else if verify_list(data, list) {
+        ret = 1;
     }
 
     ret
 }
 
-fn find_unknown(data: &[char]) -> Vec<u32> {
+fn find_unknown(data: &[char]) -> Vec<usize> {
     let mut ret = vec![];
 
     for (i, c) in data.iter().enumerate() {
         if *c == '?' {
-            ret.push(i as u32);
+            ret.push(i);
         }
     }
 
@@ -95,10 +122,10 @@ fn proc_1(data: &str) -> u32 {
     let (_, data) = parse(data).unwrap();
     let mut ret = 0;
     for d in data {
-        let (data, list) = d;
+        let (mut data, list) = d;
         let unknown_list = find_unknown(&data);
 
-        ret += solve(data, unknown_list, &list);
+        ret += solve(&mut data, &unknown_list, 0, &list);
     }
 
     ret
@@ -133,21 +160,42 @@ mod test {
 
     #[test]
     fn test_solve() {
-        let data = vec!['?', '?', '?', '.', '#', '#', '#'];
+        let mut data = vec!['?', '?', '?', '.', '#', '#', '#'];
         let list = vec![1, 1, 3];
         let unknown_list = vec![0, 1, 2];
-        let res = solve(data, unknown_list, &list);
+        let res = solve(&mut data, &unknown_list, 0, &list);
         assert_eq!(res, 1);
 
-        // let data = vec!['?','#','#','#','?','?','?','?','?','?','?', '?'];
-        // let list = vec![3,2,1];
-        let data = vec![
+        let mut data = vec![
             '.', '?', '?', '.', '.', '?', '?', '.', '.', '.', '?', '#', '#', '.',
         ];
         let list = vec![1, 1, 3];
         let unknown_list = find_unknown(&data);
-        let res = solve(data, unknown_list, &list);
+        let res = solve(&mut data, &unknown_list, 0, &list);
         assert_eq!(res, 4);
+    }
+
+    #[test]
+    fn test_verify_list() {
+        assert!(verify_list(
+            &vec!['#', '.', '#', '.', '#', '#', '#'],
+            &vec![1, 1, 3]
+        ));
+        assert!(verify_list(
+            &vec!['.', '#', '#', '#', '.', '#', '#', '.', '#', '.', '.', '.'],
+            &vec![3, 2, 1]
+        ));
+        assert!(verify_list(
+            &vec!['.', '#', '#', '#', '.', '.', '.', '#', '#', '.', '#', '.'],
+            &vec![3, 2, 1]
+        ));
+        assert_eq!(
+            verify_list(
+                &vec!['.', '#', '#', '#', '.', '.', '.', '#', '#', '.', '.', '.'],
+                &vec![3, 2, 1]
+            ),
+            false
+        );
     }
 
     #[test]
