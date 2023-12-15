57b1cb063e239b4adc8f0aafeddaea66b1c23adf
Vladimir Yakovlev <nagos@inbox.ru>
2023-12-15 11:35:16 +0300

    Day 13 part two

--- day-13/src/main.rs
+++ day-13/src/main.rs
@@ -10,8 +10,10 @@ type Block = Vec<Vec<char>>;
 
 fn main() {
     let data = fs::read_to_string("data/input.txt").unwrap();
-    let part_one = proc_1(&data);
+    let part_one = proc(&data, true);
     println!("Day 13 part one: {part_one}");
+    let part_two = proc(&data, false);
+    println!("Day 13 part two: {part_two}");
 }
 
 fn parse_line(input: &str) -> IResult<&str, Vec<char>> {
@@ -32,30 +34,29 @@ fn parse(input: &str) -> IResult<&str, Vec<Block>> {
     many1(parse_block)(input)
 }
 
-fn find_hor(data: &Block) -> Option<usize> {
+fn find_hor(data: &Block, part_one: bool) -> Option<usize> {
     let width = data[0].len();
     let height = data.len();
     for i in 0..height - 1 {
         let mut top = i;
         let mut bot = i + 1;
+        let mut diff = 0;
         loop {
-            let mut res = true;
             for q in 0..width {
                 if data[top][q] != data[bot][q] {
-                    res = false;
-                    break;
+                    diff += 1;
                 }
-            }
-            if !res {
-                break;
             }
 
             if top == 0 || bot == height - 1 {
-                return Some(i);
+                break;
             }
 
             top -= 1;
             bot += 1;
+        }
+        if (part_one && diff == 0) || (!part_one && diff == 1) {
+            return Some(i);
         }
     }
 
@@ -63,50 +64,49 @@ fn find_hor(data: &Block) -> Option<usize> {
 }
 
 #[allow(clippy::needless_range_loop)]
-fn find_ver(data: &Block) -> Option<usize> {
+fn find_ver(data: &Block, part_one: bool) -> Option<usize> {
     let width = data[0].len();
     let height = data.len();
     for i in 0..width - 1 {
         let mut left = i;
         let mut right = i + 1;
+        let mut diff = 0;
         loop {
-            let mut res = true;
             for q in 0..height {
                 if data[q][left] != data[q][right] {
-                    res = false;
-                    break;
+                    diff += 1;
                 }
-            }
-            if !res {
-                break;
             }
 
             if left == 0 || right == width - 1 {
-                return Some(i);
+                break;
             }
 
             left -= 1;
             right += 1;
+        }
+        if (part_one && diff == 0) || (!part_one && diff == 1) {
+            return Some(i);
         }
     }
 
     None
 }
 
-fn process_block(data: &Block) -> usize {
-    if let Some(x) = find_hor(data) {
+fn process_block(data: &Block, part_one: bool) -> usize {
+    if let Some(x) = find_hor(data, part_one) {
         (x + 1) * 100
-    } else if let Some(x) = find_ver(data) {
+    } else if let Some(x) = find_ver(data, part_one) {
         x + 1
     } else {
         unreachable!();
     }
 }
 
-fn proc_1(data: &str) -> usize {
+fn proc(data: &str, part_one: bool) -> usize {
     let (_, data) = parse(data).unwrap();
 
-    data.iter().map(process_block).sum()
+    data.iter().map(|b| process_block(b, part_one)).sum()
 }
 
 #[cfg(test)]
@@ -125,23 +125,56 @@ mod test {
         let data = fs::read_to_string("data/test.txt").unwrap();
         let (input, data) = parse(&data).unwrap();
         assert!(input.is_empty());
-        let res = process_block(&data[0]);
+        let res = process_block(&data[0], true);
         assert_eq!(res, 5);
-        let res = process_block(&data[1]);
+        let res = process_block(&data[1], true);
         assert_eq!(res, 400);
     }
 
     #[test]
     fn test_proc_1() {
         let data = fs::read_to_string("data/test.txt").unwrap();
-        let res = proc_1(&data);
+        let res = proc(&data, true);
         assert_eq!(res, 405);
+        let res = proc(&data, false);
+        assert_eq!(res, 400);
     }
 
     #[test]
     fn test_proc_2() {
         let data = fs::read_to_string("data/test2.txt").unwrap();
-        let res = proc_1(&data);
+        let res = proc(&data, true);
         assert_eq!(res, 100);
+    }
+
+    #[test]
+    fn test_find_hor() {
+        let data = fs::read_to_string("data/test.txt").unwrap();
+        let (input, data) = parse(&data).unwrap();
+        assert!(input.is_empty());
+        let res = find_hor(&data[0], true);
+        assert_eq!(res, None);
+        let res = find_hor(&data[1], true);
+        assert_eq!(res, Some(3));
+
+        let res = find_hor(&data[0], false);
+        assert_eq!(res, Some(2));
+        let res = find_hor(&data[1], false);
+        assert_eq!(res, Some(0));
+    }
+
+    #[test]
+    fn test_find_ver() {
+        let data = fs::read_to_string("data/test.txt").unwrap();
+        let (input, data) = parse(&data).unwrap();
+        assert!(input.is_empty());
+        let res = find_ver(&data[0], true);
+        assert_eq!(res, Some(4));
+        let res = find_ver(&data[1], true);
+        assert_eq!(res, None);
+        let res = find_ver(&data[0], false);
+        assert_eq!(res, None);
+        let res = find_ver(&data[1], false);
+        assert_eq!(res, None);
     }
 }
