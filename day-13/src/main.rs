263eeaf6c4928b3fbae7a5f6aab27c57656f8fcc
Vladimir Yakovlev <nagos@inbox.ru>
2023-12-15 12:05:01 +0300

    Refactoring

--- day-13/src/main.rs
+++ day-13/src/main.rs
@@ -34,28 +34,22 @@ fn parse(input: &str) -> IResult<&str, Vec<Block>> {
     many1(parse_block)(input)
 }
 
-fn find_hor(data: &Block, part_one: bool) -> Option<usize> {
+fn find_hor(data: &Block, target: usize) -> Option<usize> {
     let width = data[0].len();
     let height = data.len();
-    for i in 0..height - 1 {
-        let mut top = i;
-        let mut bot = i + 1;
+    let last_index = height - 1;
+    for i in 0..last_index {
         let mut diff = 0;
-        loop {
-            for q in 0..width {
-                if data[top][q] != data[bot][q] {
-                    diff += 1;
-                }
-            }
+        for j in 0.. {
+            let top = i - j;
+            let bot = i + 1 + j;
+            diff += (0..width).filter(|&q| data[top][q] != data[bot][q]).count();
 
-            if top == 0 || bot == height - 1 {
+            if top == 0 || bot == last_index {
                 break;
             }
-
-            top -= 1;
-            bot += 1;
         }
-        if (part_one && diff == 0) || (!part_one && diff == 1) {
+        if diff == target {
             return Some(i);
         }
     }
@@ -64,28 +58,24 @@ fn find_hor(data: &Block, part_one: bool) -> Option<usize> {
 }
 
 #[allow(clippy::needless_range_loop)]
-fn find_ver(data: &Block, part_one: bool) -> Option<usize> {
+fn find_ver(data: &Block, target: usize) -> Option<usize> {
     let width = data[0].len();
     let height = data.len();
-    for i in 0..width - 1 {
-        let mut left = i;
-        let mut right = i + 1;
+    let last_index = width - 1;
+    for i in 0..last_index {
         let mut diff = 0;
-        loop {
-            for q in 0..height {
-                if data[q][left] != data[q][right] {
-                    diff += 1;
-                }
-            }
+        for j in 0.. {
+            let left = i - j;
+            let right = i + 1 + j;
+            diff += (0..height)
+                .filter(|&q| data[q][left] != data[q][right])
+                .count();
 
-            if left == 0 || right == width - 1 {
+            if left == 0 || right == last_index {
                 break;
             }
-
-            left -= 1;
-            right += 1;
         }
-        if (part_one && diff == 0) || (!part_one && diff == 1) {
+        if diff == target {
             return Some(i);
         }
     }
@@ -94,9 +84,10 @@ fn find_ver(data: &Block, part_one: bool) -> Option<usize> {
 }
 
 fn process_block(data: &Block, part_one: bool) -> usize {
-    if let Some(x) = find_hor(data, part_one) {
+    let target = if part_one { 0 } else { 1 };
+    if let Some(x) = find_hor(data, target) {
         (x + 1) * 100
-    } else if let Some(x) = find_ver(data, part_one) {
+    } else if let Some(x) = find_ver(data, target) {
         x + 1
     } else {
         unreachable!();
@@ -152,14 +143,14 @@ mod test {
         let data = fs::read_to_string("data/test.txt").unwrap();
         let (input, data) = parse(&data).unwrap();
         assert!(input.is_empty());
-        let res = find_hor(&data[0], true);
+        let res = find_hor(&data[0], 0);
         assert_eq!(res, None);
-        let res = find_hor(&data[1], true);
+        let res = find_hor(&data[1], 0);
         assert_eq!(res, Some(3));
 
-        let res = find_hor(&data[0], false);
+        let res = find_hor(&data[0], 1);
         assert_eq!(res, Some(2));
-        let res = find_hor(&data[1], false);
+        let res = find_hor(&data[1], 1);
         assert_eq!(res, Some(0));
     }
 
@@ -168,13 +159,13 @@ mod test {
         let data = fs::read_to_string("data/test.txt").unwrap();
         let (input, data) = parse(&data).unwrap();
         assert!(input.is_empty());
-        let res = find_ver(&data[0], true);
+        let res = find_ver(&data[0], 0);
         assert_eq!(res, Some(4));
-        let res = find_ver(&data[1], true);
+        let res = find_ver(&data[1], 0);
         assert_eq!(res, None);
-        let res = find_ver(&data[0], false);
+        let res = find_ver(&data[0], 1);
         assert_eq!(res, None);
-        let res = find_ver(&data[1], false);
+        let res = find_ver(&data[1], 1);
         assert_eq!(res, None);
     }
 }
