9f91469bb05fc0e2a18b679318590b4bf0efd2cc
Vladimir Yakovlev <nagos@inbox.ru>
2023-12-15 21:45:19 +0300

    Day 15 part two

--- day-15/src/main.rs
+++ day-15/src/main.rs
@@ -1,7 +1,9 @@
 use nom::{
-    bytes::complete::tag,
-    character::complete::{newline, none_of},
-    multi::{many1, separated_list1},
+    bytes::complete::{tag, take_till1},
+    character::complete::{alphanumeric1, digit1, newline},
+    combinator::map_res,
+    combinator::opt,
+    multi::separated_list1,
     IResult,
 };
 
@@ -11,35 +13,96 @@ fn main() {
     let data = fs::read_to_string("data/input.txt").unwrap();
     let part_one = proc(&data);
     println!("Day 15 part one: {part_one}");
+    let part_two = proc_2(&data);
+    println!("Day 15 part two: {part_two}");
 }
 
-fn parse_block(input: &str) -> IResult<&str, Vec<char>> {
-    many1(none_of(",\n"))(input)
+fn parse_block(input: &str) -> IResult<&str, &str> {
+    take_till1(|c| c == ',' || c == '\n')(input)
 }
 
-fn parse(input: &str) -> IResult<&str, Vec<Vec<char>>> {
+fn parse(input: &str) -> IResult<&str, Vec<&str>> {
     let (input, data) = separated_list1(tag(","), parse_block)(input)?;
     let (input, _) = newline(input)?;
 
     Ok((input, data))
 }
 
-fn hash(data: &Vec<char>) -> u32 {
-    let mut ret = 0;
+fn digit1_u32(input: &str) -> IResult<&str, u32> {
+    map_res(digit1, |s: &str| s.parse::<u32>())(input)
+}
 
-    for &d in data {
-        ret += d as u32;
-        ret *= 17;
-        ret %= 256;
+fn parse_value(input: &str) -> IResult<&str, u32> {
+    let (input, _) = tag("=")(input)?;
+    let (input, value) = digit1_u32(input)?;
+
+    Ok((input, value))
+}
+
+fn parse_part2(input: &str) -> IResult<&str, (&str, Option<u32>)> {
+    let (input, label) = alphanumeric1(input)?;
+    let (mut input, value) = opt(parse_value)(input)?;
+
+    if value.is_none() {
+        (input, _) = tag("-")(input)?;
     }
 
-    ret
+    Ok((input, (label, value)))
+}
+
+fn hash(data: &str) -> u32 {
+    data.chars().fold(0, |acc, x| ((acc + x as u32) * 17) % 256)
 }
 
 fn proc(data: &str) -> u32 {
     let (_, data) = parse(data).unwrap();
 
-    data.iter().map(hash).sum()
+    data.iter().map(|&s| hash(s)).sum()
+}
+
+fn box_find_lens(store: &[(&str, u32)], label: &str) -> Option<usize> {
+    for (i, v) in store.iter().enumerate() {
+        if v.0 == label {
+            return Some(i);
+        }
+    }
+    None
+}
+
+fn proc_2(data: &str) -> usize {
+    let mut store: Vec<Vec<(&str, u32)>> = vec![vec![]; 256];
+    let (_, data) = parse(data).unwrap();
+
+    for l in data {
+        let (_, (label, value)) = parse_part2(l).unwrap();
+        let h = hash(label);
+        let lens_box = &mut store[h as usize];
+        let idx = box_find_lens(lens_box, label);
+
+        match (value, idx) {
+            (Some(val), Some(idx)) => {
+                lens_box[idx].1 = val;
+            }
+            (Some(val), None) => {
+                lens_box.push((label, val));
+            }
+            (None, Some(idx)) => {
+                lens_box.remove(idx);
+            }
+            _ => {}
+        };
+    }
+
+    store
+        .iter()
+        .enumerate()
+        .map(|(i, b)| {
+            b.iter()
+                .enumerate()
+                .map(|(q, lens)| (i + 1) * (q + 1) * (lens.1 as usize))
+                .sum::<usize>()
+        })
+        .sum()
 }
 
 #[cfg(test)]
@@ -55,8 +118,7 @@ mod test {
 
     #[test]
     fn test_hash() {
-        let data = vec!['H', 'A', 'S', 'H'];
-
+        let data = "HASH";
         let res = hash(&data);
         assert_eq!(res, 52);
     }
@@ -66,5 +128,20 @@ mod test {
         let data = fs::read_to_string("data/test.txt").unwrap();
         let res = proc(&data);
         assert_eq!(res, 1320);
+    }
+
+    #[test]
+    fn test_parse_2() {
+        let data = "rn=1";
+        let (input, data) = parse_part2(&data).unwrap();
+        assert!(input.is_empty());
+        assert_eq!(data.1, Some(1))
+    }
+
+    #[test]
+    fn test_proc_2() {
+        let data = fs::read_to_string("data/test.txt").unwrap();
+        let res = proc_2(&data);
+        assert_eq!(res, 145);
     }
 }
