0b04d9c76e1f2052505b54de8b134658b8748712
Vladimir Yakovlev <nagos@inbox.ru>
2023-12-16 18:11:10 +0300

    Day 16 part one

---
+++ day-16/src/main.rs
@@ -0,0 +1,185 @@
+use nom::{
+    character::complete::{newline, one_of},
+    multi::{many0, many1},
+    IResult,
+};
+
+use std::fs;
+
+fn main() {
+    let data = fs::read_to_string("data/input.txt").unwrap();
+    let part_one = calc_1(&data);
+    println!("Day 16 part one: {part_one}");
+}
+
+#[derive(Debug)]
+enum Dir {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+fn parse_line(input: &str) -> IResult<&str, Vec<char>> {
+    let (input, data) = many1(one_of("./\\-|"))(input)?;
+    let (input, _) = newline(input)?;
+
+    Ok((input, data))
+}
+
+fn parse(input: &str) -> IResult<&str, Vec<Vec<char>>> {
+    let (input, data) = many1(parse_line)(input)?;
+    let (input, _) = many0(newline)(input)?;
+
+    Ok((input, data))
+}
+
+fn beam_move(data: Vec<Vec<char>>) -> u32 {
+    let mut queue: Vec<(Dir, usize, usize)> = vec![];
+    let width = data[0].len();
+    let height = data.len();
+
+    fn add_left(x: usize, y: usize, queue: &mut Vec<(Dir, usize, usize)>) {
+        if x != 0 {
+            queue.push((Dir::Left, x - 1, y));
+        }
+    }
+
+    fn add_up(x: usize, y: usize, queue: &mut Vec<(Dir, usize, usize)>) {
+        if y != 0 {
+            queue.push((Dir::Up, x, y - 1));
+        }
+    }
+    fn add_down(x: usize, y: usize, queue: &mut Vec<(Dir, usize, usize)>, height: usize) {
+        if y != height - 1 {
+            queue.push((Dir::Down, x, y + 1));
+        }
+    }
+    fn add_right(x: usize, y: usize, queue: &mut Vec<(Dir, usize, usize)>, width: usize) {
+        if x != width - 1 {
+            queue.push((Dir::Right, x + 1, y));
+        }
+    }
+
+    let mut visit_list = vec![vec![(false, false, false, false); width]; height];
+
+    queue.push((Dir::Right, 0, 0));
+
+    while let Some((d, x, y)) = queue.pop() {
+        let visited = match d {
+            Dir::Up => visit_list[y][x].0,
+            Dir::Down => visit_list[y][x].1,
+            Dir::Left => visit_list[y][x].2,
+            Dir::Right => visit_list[y][x].3,
+        };
+        if visited {
+            continue;
+        }
+        match d {
+            Dir::Up => visit_list[y][x].0 = true,
+            Dir::Down => visit_list[y][x].1 = true,
+            Dir::Left => visit_list[y][x].2 = true,
+            Dir::Right => visit_list[y][x].3 = true,
+        };
+        let c = data[y][x];
+        match (d, c) {
+            (Dir::Right, '.') => add_right(x, y, &mut queue, width),
+            (Dir::Right, '|') => {
+                add_up(x, y, &mut queue);
+                add_down(x, y, &mut queue, height);
+            }
+            (Dir::Right, '-') => add_right(x, y, &mut queue, width),
+            (Dir::Right, '\\') => {
+                add_down(x, y, &mut queue, height);
+            }
+            (Dir::Right, '/') => {
+                add_up(x, y, &mut queue);
+            }
+            (Dir::Down, '.') => {
+                add_down(x, y, &mut queue, height);
+            }
+            (Dir::Down, '|') => {
+                add_down(x, y, &mut queue, height);
+            }
+            (Dir::Down, '\\') => {
+                add_right(x, y, &mut queue, width);
+            }
+            (Dir::Down, '/') => {
+                add_left(x, y, &mut queue);
+            }
+            (Dir::Down, '-') => {
+                add_left(x, y, &mut queue);
+                add_right(x, y, &mut queue, width);
+            }
+            (Dir::Up, '/') => {
+                add_right(x, y, &mut queue, width);
+            }
+            (Dir::Up, '|') => {
+                add_up(x, y, &mut queue);
+            }
+            (Dir::Up, '.') => {
+                add_up(x, y, &mut queue);
+            }
+            (Dir::Up, '\\') => {
+                add_left(x, y, &mut queue);
+            }
+            (Dir::Up, '-') => {
+                add_left(x, y, &mut queue);
+                add_right(x, y, &mut queue, width);
+            }
+            (Dir::Left, '\\') => {
+                add_up(x, y, &mut queue);
+            }
+            (Dir::Left, '/') => {
+                add_down(x, y, &mut queue, height);
+            }
+            (Dir::Left, '|') => {
+                add_up(x, y, &mut queue);
+                add_down(x, y, &mut queue, height);
+            }
+            (Dir::Left, '.') => {
+                add_left(x, y, &mut queue);
+            }
+            (Dir::Left, '-') => {
+                add_left(x, y, &mut queue);
+            }
+            _ => unreachable!(),
+        }
+    }
+
+    let mut ret = 0;
+
+    for y in 0..height {
+        for x in 0..width {
+            let visited = visit_list[y][x];
+            if visited.0 || visited.1 || visited.2 || visited.3 {
+                ret += 1;
+            }
+        }
+    }
+
+    ret
+}
+
+fn calc_1(data: &str) -> u32 {
+    let (_, data) = parse(data).unwrap();
+    beam_move(data)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_parse() {
+        let data = fs::read_to_string("data/test.txt").unwrap();
+        let (input, _data) = parse(&data).unwrap();
+        assert!(input.is_empty());
+    }
+
+    #[test]
+    fn test_calc1() {
+        let data = fs::read_to_string("data/test.txt").unwrap();
+        let res = calc_1(&data);
+        assert_eq!(res, 46);
+    }
+}
