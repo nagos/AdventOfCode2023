c9ee5d4b60149b8ea90b7577a0b10aac12d841f0
Vladimir Yakovlev <nagos@inbox.ru>
2023-12-17 19:13:50 +0300

    Refactoring

--- day-17/src/main.rs
+++ day-17/src/main.rs
@@ -5,7 +5,7 @@ use nom::{
     IResult,
 };
 
-use std::collections::{BinaryHeap, HashMap, HashSet};
+use std::collections::{BinaryHeap, HashMap};
 use std::{fs, vec};
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
@@ -16,13 +16,18 @@ enum Dir {
     Right,
 }
 
-#[derive(Debug, PartialEq, Eq)]
-struct PathNode {
-    score: u32,
+#[derive(Debug, PartialEq, Clone, Copy, Eq, Hash, PartialOrd, Ord)]
+struct NodePos {
     x: u32,
     y: u32,
     dir: Dir,
     cons: u32,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+struct PathNode {
+    score: u32,
+    pos: NodePos,
 }
 
 impl PartialOrd for PathNode {
@@ -59,7 +64,7 @@ fn parse(input: &str) -> IResult<&str, Vec<Vec<u32>>> {
     Ok((input, data))
 }
 
-fn get_next_dir(node: &PathNode, width: usize, height: usize) -> Vec<Dir> {
+fn get_next_dir(node: &NodePos, width: usize, height: usize) -> Vec<Dir> {
     let mut ret = vec![];
     match node.dir {
         Dir::Down => {
@@ -88,7 +93,7 @@ fn get_next_dir(node: &PathNode, width: usize, height: usize) -> Vec<Dir> {
             if node.x != 0 && node.cons != 3 {
                 ret.push(Dir::Left)
             }
-            if node.y != 0 as u32 {
+            if node.y != 0 {
                 ret.push(Dir::Up)
             }
             if node.y != height as u32 - 1 {
@@ -99,7 +104,7 @@ fn get_next_dir(node: &PathNode, width: usize, height: usize) -> Vec<Dir> {
             if node.x != width as u32 - 1 && node.cons != 3 {
                 ret.push(Dir::Right)
             }
-            if node.y != 0 as u32 {
+            if node.y != 0 {
                 ret.push(Dir::Up)
             }
             if node.y != height as u32 - 1 {
@@ -110,72 +115,30 @@ fn get_next_dir(node: &PathNode, width: usize, height: usize) -> Vec<Dir> {
     ret
 }
 
-fn get_next_node(node: &PathNode, dir: Dir, data: &Vec<Vec<u32>>) -> PathNode {
+fn get_next_node(node: &PathNode, dir: Dir, data: &[Vec<u32>]) -> PathNode {
     let (new_x, new_y) = match dir {
-        Dir::Down => (node.x, node.y + 1),
-        Dir::Left => (node.x - 1, node.y),
-        Dir::Right => (node.x + 1, node.y),
-        Dir::Up => (node.x, node.y - 1),
+        Dir::Down => (node.pos.x, node.pos.y + 1),
+        Dir::Left => (node.pos.x - 1, node.pos.y),
+        Dir::Right => (node.pos.x + 1, node.pos.y),
+        Dir::Up => (node.pos.x, node.pos.y - 1),
     };
 
     let new_score = node.score + data[new_y as usize][new_x as usize];
-    let new_cons = if dir == node.dir { node.cons + 1 } else { 1 };
+    let new_cons = if dir == node.pos.dir {
+        node.pos.cons + 1
+    } else {
+        1
+    };
 
     PathNode {
         score: new_score,
-        x: new_x,
-        y: new_y,
-        dir: dir,
-        cons: new_cons,
+        pos: NodePos {
+            x: new_x,
+            y: new_y,
+            dir,
+            cons: new_cons,
+        },
     }
-}
-
-fn reconstruct_path(
-    x: u32,
-    y: u32,
-    d: Dir,
-    cons: u32,
-    came_from: &HashMap<(u32, u32, Dir, u32), (u32, u32, Dir, u32)>,
-    data: &Vec<Vec<u32>>,
-) {
-    let mut x = x;
-    let mut y = y;
-    let mut d = d;
-    let mut cons = cons;
-
-    println!("{x} {y} {}", data[y as usize][x as usize]);
-    let mut m = HashSet::new();
-    let mut score = data[y as usize][x as usize];
-    m.insert((x, y));
-    while let Some(&(next_x, next_y, next_d, next_cons)) = came_from.get(&(x, y, d, cons)) {
-        score += data[y as usize][x as usize];
-        x = next_x;
-        y = next_y;
-        d = next_d;
-        cons = next_cons;
-        m.insert((x, y));
-        println!("{x} {y} {}", data[y as usize][x as usize]);
-    }
-
-    dbg!(score - data[0][0]);
-    for y in 0..data.len() {
-        for x in 0..data[0].len() {
-            if m.contains(&(x as u32, y as u32)) {
-                print!("x");
-            } else {
-                print!("{}", data[y][x]);
-            }
-        }
-        println!();
-    }
-}
-
-fn resort_heap<T: Ord>(b: &mut BinaryHeap<T>) {
-    use std::mem::swap;
-    let mut temp = BinaryHeap::new();
-    swap(b, &mut temp);
-    temp = BinaryHeap::from(temp);
-    swap(b, &mut temp);
 }
 
 fn pathfind(data: Vec<Vec<u32>>) -> u32 {
@@ -183,58 +146,46 @@ fn pathfind(data: Vec<Vec<u32>>) -> u32 {
     let height = data.len();
     let (target_x, target_y) = (width as u32 - 1, height as u32 - 1);
     let mut open_set = BinaryHeap::new();
-    let mut visited: HashMap<(u32, u32, Dir, u32), u32> = HashMap::new();
-    let mut came_from: HashMap<(u32, u32, Dir, u32), (u32, u32, Dir, u32)> = HashMap::new();
+    let mut visited: HashMap<NodePos, u32> = HashMap::new();
+    let mut came_from: HashMap<NodePos, NodePos> = HashMap::new();
 
     let start = PathNode {
-        cons: 0,
         score: 0,
-        x: 0,
-        y: 0,
-        dir: Dir::Right,
+        pos: NodePos {
+            cons: 0,
+            x: 0,
+            y: 0,
+            dir: Dir::Right,
+        },
     };
 
-    visited.insert((0, 0, Dir::Right, 0), 0);
+    visited.insert(start.pos, start.score);
     open_set.push(start);
 
     let mut found = None;
 
     while let Some(node) = open_set.pop() {
-        if node.x == target_x && node.y == target_y {
-            found = Some((node.x, node.y, node.dir, node.cons, node.score));
+        if node.pos.x == target_x && node.pos.y == target_y {
+            found = Some(node);
             break;
         }
 
-        for dir in get_next_dir(&node, width, height) {
+        for dir in get_next_dir(&node.pos, width, height) {
             let next_node = get_next_node(&node, dir, &data);
-            if let Some(v) =
-                visited.get_mut(&(next_node.x, next_node.y, next_node.dir, next_node.cons))
-            {
+            if let Some(v) = visited.get_mut(&next_node.pos) {
                 if *v > next_node.score {
                     *v = next_node.score;
-                    came_from.insert(
-                        (next_node.x, next_node.y, next_node.dir, next_node.cons),
-                        (node.x, node.y, node.dir, node.cons),
-                    );
-                    open_set.push(next_node);
-                    resort_heap(&mut open_set);
+                    came_from.insert(next_node.pos, node.pos);
                 }
             } else {
-                visited.insert(
-                    (next_node.x, next_node.y, next_node.dir, next_node.cons),
-                    next_node.score,
-                );
-                came_from.insert(
-                    (next_node.x, next_node.y, next_node.dir, next_node.cons),
-                    (node.x, node.y, node.dir, node.cons),
-                );
+                visited.insert(next_node.pos, next_node.score);
+                came_from.insert(next_node.pos, node.pos);
                 open_set.push(next_node);
-                resort_heap(&mut open_set);
             }
         }
     }
 
-    found.unwrap().4
+    found.unwrap().score
 }
 
 fn proc_1(data: &str) -> u32 {
